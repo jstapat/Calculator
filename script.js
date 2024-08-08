a9b1606c92960c15b326a527175925f012523198
Jonathan Stapat <jonathanstapat@gmx.de>
2024-08-08 16:59:41 +0200

    Added const for each number

---
+++ script.js
@@ -0,0 +1,19 @@
+const one = 1;
+const two = 2;
+const three = 3;
+const four = 4;
+const five = 5;
+const six = 6;
+const seven = 7;
+const eight = 8;
+const nine = 9;
+const zero = 0;
+const plus = "+";
+const minus = "-";
+const division = "/";
+const multiplikation = "*";
+const equals = "=";
+
+var calculation = "";
+
+function input7() {}
