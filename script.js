48385155a4ba3f272fd7150dd957b0c300676e9d
Jonathan <jonathanstapat@gmx.de>
2024-08-18 14:27:54 +0200

    added history

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let newInput = 0;
-let lastInput = 0;
+let lastResult = 0;
+let currentResult = 0;
 let operator = "+";
+let liste = "";
 
 function numberInput(number) {
   newInput = newInput * 10 + number;
@@ -10,40 +12,45 @@ function numberInput(number) {
 function add() {
   operation();
   operator = "+";
-  document.getElementById("calculation").innerHTML = lastInput + operator;
+  document.getElementById("calculation").innerHTML = currentResult + operator;
 }
 
 function subtract() {
   operation();
   operator = "-";
-  document.getElementById("calculation").innerHTML = lastInput + operator;
+  document.getElementById("calculation").innerHTML = currentResult + operator;
 }
 
 function multiply() {
   operation();
   operator = "*";
-  document.getElementById("calculation").innerHTML = lastInput + operator;
+  document.getElementById("calculation").innerHTML = currentResult + operator;
 }
 
 function divide() {
   operation();
   operator = "/";
-  document.getElementById("calculation").innerHTML = lastInput + operator;
+  document.getElementById("calculation").innerHTML = currentResult + operator;
 }
 
 function operation() {
   if (operator === "+") {
-    lastInput = lastInput + newInput;
+    currentResult = currentResult + newInput;
   } else if (operator === "-") {
-    lastInput = lastInput - newInput;
+    currentResult = currentResult - newInput;
   } else if (operator === "*") {
-    lastInput = lastInput * newInput;
+    currentResult = currentResult * newInput;
   } else if (operator === "/") {
-    lastInput = lastInput / newInput;
+    currentResult = currentResult / newInput;
   }
-
-  newInput = 0;
   document.getElementById("button-input").innerHTML = 0;
+
+  let listeString = lastResult + operator + newInput + "=" + currentResult;
+  liste = "<p>"+ listeString + liste +"</p>";
+  updateResult();
+  
+  lastResult = newInput;
+  newInput = 0;
 }
 
 function equals() {}
@@ -51,7 +58,12 @@ function equals() {}
 function clearAll() {
   newInput = 0;
   document.getElementById("button-input").innerHTML = 0;
-  lastInput = 0;
+  lastResult = 0;
   document.getElementById("calculation").innerHTML = 0;
+  liste = 0;
   document.getElementById("results").innerHTML = 0;
+}
+
+function updateResult() {
+  document.getElementById("results").innerHTML = liste;
 }
