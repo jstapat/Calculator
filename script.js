86bc4b81d87197acf59ab8841066fce1d00b7599
Jonathan Stapat <jonathanstapat@gmx.de>
2024-08-19 08:47:25 +0200

    Fixed the history part

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let newInput = 0;
-let lastResult = 0;
+let lastInput = 0;
 let currentResult = 0;
 let operator = "+";
 let liste = "";
@@ -35,30 +35,40 @@ function divide() {
 
 function operation() {
   if (operator === "+") {
-    currentResult = currentResult + newInput;
+    currentResult = lastInput + newInput;
   } else if (operator === "-") {
-    currentResult = currentResult - newInput;
+    currentResult = lastInput - newInput;
   } else if (operator === "*") {
-    currentResult = currentResult * newInput;
+    currentResult = lastInput * newInput;
   } else if (operator === "/") {
-    currentResult = currentResult / newInput;
+    currentResult = lastInput / newInput;
   }
   document.getElementById("button-input").innerHTML = 0;
 
-  let listeString = lastResult + operator + newInput + "=" + currentResult;
-  liste = "<p>"+ listeString + liste +"</p>";
+  let listeString =
+    "<p>" + lastInput + operator + newInput + "=" + currentResult + "</p>";
+  liste = listeString + liste;
+  lastInput = currentResult;
   updateResult();
-  
-  lastResult = newInput;
   newInput = 0;
 }
 
-function equals() {}
+function equals() {
+  let listeString =
+    "<p>" + lastInput + operator + newInput + "=" + currentResult + "</p>";
+  liste = listeString + liste;
+  updateResult();
+  newInput = 0;
+  document.getElementById("button-input").innerHTML = 0;
+  lastInput = 0;
+  document.getElementById("calculation").innerHTML = 0;
+  operator = "+";
+}
 
 function clearAll() {
   newInput = 0;
   document.getElementById("button-input").innerHTML = 0;
-  lastResult = 0;
+  lastInput = 0;
   document.getElementById("calculation").innerHTML = 0;
   liste = 0;
   document.getElementById("results").innerHTML = 0;
